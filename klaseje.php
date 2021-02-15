3685d8b7fc239a81c7e4688e4c02e6853e77f8f9
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-02-15 17:23:39 +0200

    Initial

---
+++ klaseje.php
@@ -0,0 +1,104 @@
+<?php
+$masyvas =[];
+$masyvas['knygos'] = '2knygos';
+$masyvas['zurnalai'] = 'seni zurnalai';
+$masyvas[''] = 'kates zaislai';
+$masyvas[5]= 'knyga';
+echo '<pre>';
+echo var_dump($masyvas);
+echo '</pre>';
+
+
+$masyvas2 = [
+    'mano knygos',
+    'zurnalai' => 'seni zurnalai',
+    'zaislai' => 'kates zaislai',
+    'mano batai'
+];
+
+echo '<pre>';
+foreach($masyvas2 as $index => $value) {
+    if('seni zurnalai' == $value) {
+        $masyvas2[$index] = 'Dar daugiau kates zaislu';
+    }
+}
+
+
+// // echo '<pre>';
+// // print_r($masyvas2);
+
+// $masyvas3['kojines']['zalios'] = 'tik zalios kojines';
+// $masyvas3['kojines']['juodos'] = 'tik juodos kojines';
+// $masyvas3['kojines']['baltos'] = 'tik baltos kojines';
+// print_r($masyvas3);
+
+
+// // foreach($masyvas as $index => $value) {
+// //     echo '<br>';
+// //     echo $index . ' => ' . $value;
+// // }
+// // echo '<br>';
+// // foreach(range('A', 'K') as $value) {
+// //     echo '<br>';
+// //     echo $value;
+// // }
+
+// // echo '<br>';
+// // $m = range(1, 5);
+// // shuffle($m);
+// // print_r($m);
+
+// // echo '<br>';
+// // echo current($m);
+// // echo '<br>';
+// // echo next($m);
+// // echo '<br>';
+// // echo current($m);
+
+// $m = ['red', 'green', 'yellow', 'blue'];
+// // $A = 5;
+// // $B = &$A; //& variable is passed by reference(nuoroda) (and not by value) !!!
+// // $A = 17;
+
+// // echo '<br>';
+// // echo $B;
+
+// foreach($m as &$value) {}
+
+// unset($value);
+
+// //priskyrimas pagal reference and value
+
+// foreach($m as $value){
+//     echo '<br>';
+//     echo $value;
+// }
+// // echo '<br>';
+// // echo current($m);
+
+
+
+// for ($i = 1;$i <= 15;$i++){
+//     if (rand(0, 10)> 9){
+//         break;
+//     }
+//     echo $i;
+//     echo '<br>';
+//   }
+//   echo 'Ciklo pabaiga <br>';
+
+
+//   $i = 0;
+// for ($i = 0;$i <= 5;$i++){
+//    if ($i==2){
+//        continue;
+//    }
+//    echo $i;
+//    echo '<br>';
+// }
+// echo 'Ciklo pabaiga';
+
+
+
+// //budai pakeisti kintamaji
+// ?>
